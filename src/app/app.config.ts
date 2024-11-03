e32c71b97bd12b529de0ea91686c986ed73faa8f
Pedro Coelho <tp.pedroh.silva@d0007azurecloudtotvscom.onmicrosoft.com>
2024-11-03 00:50:41 -0300

    refactor(index): change material icons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { MAT_ICON_DEFAULT_OPTIONS } from '@angular/material/icon';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes), provideAnimationsAsync(),
+    provideRouter(appRoutes),
+    provideAnimationsAsync(),
+    {
+      provide: MAT_ICON_DEFAULT_OPTIONS,
+      useValue: { fontSet: 'material-icons-outlined' },
+    },
   ],
 };
